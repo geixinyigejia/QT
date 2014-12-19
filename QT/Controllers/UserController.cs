60d5448c55bd043dfc5d5e02d3a8457ff58ac0d6
geixinyigejia <wanyandong123@hotmail.com>
2014-12-19 23:11:12 +0800

    modify the user info and suggestion area

--- QT/Controllers/UserController.cs
+++ QT/Controllers/UserController.cs
@@ -4,16 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+//
+using System.Data;
+using System.Data.SqlClient;
+using GetLBAMVC.Models;
+using System.Reflection;
+using PagedList;
+
 namespace GetLBAMVC.Controllers
 {
     public class UserController : Controller
     {
-        //
-        // GET: /User/
+        private static string DBName = "QT";
 
         public ActionResult Index()
         {
-            return View();
+            GetLBAMVC.Models.User userInfo = GetcurrentUserInfo(User.Identity.Name);
+            return View(userInfo);
         }
 
         public ActionResult TaskRecords()
@@ -25,9 +32,93 @@ namespace GetLBAMVC.Controllers
         {
             return View();
         }
-        public ActionResult Suggestions()
-        {
-            return View();
+        public ActionResult Suggestions( int p = 1)
+        {           
+            List<Suggestion> suggesions = GetAllSuggestions();
+            var pagedData = suggesions.ToPagedList(pageNumber: p, pageSize: 20);
+            return View(pagedData);
         }
+        
+        [HttpPost]
+        public ActionResult Suggestions(string Comment,int p=1)
+        {
+            if(!string.IsNullOrEmpty(Comment))
+            {
+                InsertSuggestionToDB(Comment);
+            }
+            List<Suggestion> suggesions = GetAllSuggestions();
+            var pagedData = suggesions.ToPagedList(pageNumber: p, pageSize: 20);
+            return View(pagedData); 
+        }
+
+        #region 辅助方法
+        private User GetcurrentUserInfo(string username)
+        {
+            SqlConnection sqlconn = commonContext.connectonToMSSQL();
+            string sqlCommand = string.Format(@"use {0}; select * from QT_USER  where UserName='{1}' ", DBName, username);
+            sqlconn.Open();
+            SqlCommand cmd = new SqlCommand(sqlCommand, sqlconn);
+            
+            SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            User user = new User();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    PropertyInfo property = user.GetType().GetProperty(reader.GetName(i));
+                    property.SetValue(user, reader.IsDBNull(i) ? "[null]" : reader.GetValue(i), null);
+                }
+            }
+            reader.Close();
+            return user;
+        }
+
+        private List<Suggestion> GetAllSuggestions()
+        {
+            List<Suggestion> suggestions = new List<Suggestion>();
+
+            SqlConnection sqlconn = commonContext.connectonToMSSQL();
+            string sqlCommand = string.Format(@"use {0}; select  * from QT_Suggestion order by PublishTime;", DBName);
+            sqlconn.Open();
+            SqlCommand cmd = new SqlCommand(sqlCommand, sqlconn);
+
+            SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+           
+            while (reader.Read())
+            {
+                Suggestion suggestion = new Suggestion();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    PropertyInfo property = suggestion.GetType().GetProperty(reader.GetName(i));
+                    property.SetValue(suggestion, reader.IsDBNull(i) ? "[null]" : reader.GetValue(i), null);                   
+                }
+                suggestions.Add(suggestion);
+            }
+            reader.Close();
+            return suggestions;
+        }
+
+        private void InsertSuggestionToDB(string  comment)
+        {
+            List<Suggestion> suggestions = new List<Suggestion>();
+
+            SqlConnection sqlconn = commonContext.connectonToMSSQL();
+            string sqlCommand = string.Format(@"use {0}; insert QT_Suggestion(PublishUserName,PublishTime,Content) values(N'{1}','{2}',N'{3}');", DBName,User.Identity.Name,DateTime.Now,comment);
+            sqlconn.Open();
+            SqlCommand cmd = new SqlCommand(sqlCommand, sqlconn);           
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sqlconn.Close();
+            }
+        }
+        #endregion 
     }
 }
---
+++ QT/Models/Suggestion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//added new using 
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace GetLBAMVC.Models
+{
+    public class Suggestion
+    {
+       
+        /// <summary>
+        /// 发布反馈建议的用户名
+        /// </summary>  
+        [DisplayName("发布人")]
+        public string PublishUserName
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///发布时间
+        /// </summary>
+        [DisplayName("发布时间")]
+        public DateTime PublishTime
+        {
+            get;
+            set;
+        }
+
+        [DisplayName("发布内容")]
+        public string Content
+        {
+            get;
+            set;
+        }
+    }
+}
